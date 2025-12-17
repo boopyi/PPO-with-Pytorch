0f9342d2221ff27c4301ea05c3c6f505cd88a09c
boopyi <etypity@gmail.com>
2025-12-17 19:40:35 +0200

    Refactor code structure for improved readability and maintainability

--- continuous.py
+++ continuous.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 import gymnasium as gym
-env_name="Reacher-v5"
+env_name="HalfCheetah-v5"
 env=gym.make(env_name)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -30,15 +30,15 @@ class Actor(nn.Module):
     def __init__(self):
         super().__init__()
         self.net=nn.Sequential(
-            nn.Linear(env.observation_space.shape[0],64),
-            nn.ReLU(),
-            nn.Linear(64,64),
-            nn.ReLU(),
-            nn.Linear(64,64),
-            nn.ReLU()
+            nn.Linear(env.observation_space.shape[0],512 ),
+            nn.Tanh(),
+            nn.Linear(512 ,512 ),
+            nn.Tanh(),
+            nn.Linear(512 ,512 ),
+            nn.Tanh()
             )
-        self.mu_head=nn.Linear(64,env.action_space.shape[0])
-        self.log_std = nn.Parameter(torch.zeros(1))
+        self.mu_head=nn.Linear(512 ,env.action_space.shape[0])
+        self.log_std = nn.Parameter(torch.zeros(env.action_space.shape[0]))
     def forward(self,x):
         x=self.net(x)
         mu=self.mu_head(x)
@@ -50,11 +50,11 @@ class Critic(nn.Module):
     def __init__(self):
         super().__init__()
         self.net=nn.Sequential(
-            nn.Linear(env.observation_space.shape[0],64),
+            nn.Linear(env.observation_space.shape[0],512 ),
             nn.ReLU(),
-            nn.Linear(64,64),
+            nn.Linear(512 ,512 ),
             nn.ReLU(),
-            nn.Linear(64,1)
+            nn.Linear(512 ,1)
             )
     def forward(self,x):
         return self.net(x)
@@ -62,23 +62,25 @@ def compute_advanteges(values,rewards,terminateds,trunkateds,last_value,gamma=0.
     advanteges=[]
     advantege=0
     values = torch.cat([values, last_value.unsqueeze(0)])
+    rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
     for t in reversed(range(len(values)-1)):
+        done=float(terminateds[t])
         
-        delta=rewards[t]+gamma*values[t+1]*(1-terminateds[t])-values[t]
-        done=terminateds[t]or trunkateds[t]
+        delta=rewards[t]+gamma*values[t+1]*(1-done)-values[t]
         advantege=delta+gamma*lam*(1-done)*advantege
-        advanteges.insert(0,advantege)
-    return torch.tensor(advanteges,dtype=torch.float32).to(device=device)
+        advanteges.append(advantege)
+    advanteges_tensor = torch.stack(advanteges[::-1])
+    return advanteges_tensor.to(device=device)
 
 policy=Actor()
 value_func=Critic()
 normilizer=obs_normalizer()
 
-policy_optimizer = torch.optim.Adam(policy.parameters(), lr=3e-4)
+policy_optimizer = torch.optim.Adam(policy.parameters(), lr=1e-4)
 value_optimizer = torch.optim.Adam(value_func.parameters(), lr=1e-3)
 policy.to(device)
 value_func.to(device)
-def update_policy(states, actions, log_probs_old, returns, advantages,batch_size=64,epochs=5,clip_ratio=0.2,c1=0.5,c2=0.01):
+def update_policy(states, actions, log_probs_old, returns, advantages,batch_size=1024,epochs=10,clip_ratio=0.2,c1=0.5,c2=0.02):
     dataset_size = len(states)
     for _ in range(epochs):
         indices = torch.randperm(dataset_size)
@@ -112,83 +114,100 @@ def update_policy(states, actions, log_probs_old, returns, advantages,batch_size
 
 num_episodes=500
 
-task=input("train or test?")
+task=input("train or test?\n")
+
 if  task=="train":
-    episode_count = 0
-    for _ in range(num_episodes):
-        states=[]
-        actions=[]
-        log_probs=[]
-        rewards=[]
-        terminateds=[]
-        trunkateds=[]
-        values=[]
+    try:
+        episode_count = 0
+        for _ in range(num_episodes):
+            states=[]
+            actions=[]
+            log_probs=[]
+            rewards=[]
+            terminateds=[]
+            trunkateds=[]
+            values=[]
 
-        obs,_=env.reset()
-        episode_reward = 0
-        low = torch.tensor(env.action_space.low).to(device)
-        high = torch.tensor(env.action_space.high).to(device)
-        for i in range(1024):
-            obs=torch.tensor(obs,dtype=torch.float32).to(device=device)
-            obs=normilizer.update(obs)
-            mu,std=policy.forward(obs)
-            dist=torch.distributions.Normal(mu,std)
-            action=dist.sample()
-            
-            
-            log_prob = dist.log_prob(action).sum(dim=-1)
-            value=value_func.forward(obs)
-            states.append(obs)
-            actions.append(action.cpu().numpy())
-            log_probs.append(log_prob.detach())
-            values.append(value.squeeze().detach())
-            action_env = torch.clamp(action, low, high).cpu().numpy()
-            obs, reward, terminated, truncated, _=env.step(action_env)
-            
-            rewards.append(reward)
-            episode_reward+=reward
-            trunkateds.append(truncated)
-            terminateds.append(terminated)
-            if terminated or truncated:
-                
-                episode_count += 1
-                
-                obs,_=env.reset()
-                if episode_count%20==0:
-                    print(f"Episode {episode_count} reward: {episode_reward}")
-                episode_reward = 0
-
-
-        values = torch.stack(values)
-        last_obs_tensor = normilizer.update(torch.tensor(obs, dtype=torch.float32).to(device))
-        with torch.no_grad():
-            last_value = value_func(last_obs_tensor).squeeze()
-
-        advantages=compute_advanteges(values,rewards,terminateds,trunkateds,last_value)
-        returns = advantages + values
-        advantages=(advantages-advantages.mean())/(advantages.std()+1e-8)
-        states=torch.stack(states)
-        actions = torch.from_numpy(np.array(actions)).to(torch.float32).to(device)
-        log_probs=torch.stack(log_probs)
-        policy.to(device)
-        value_func.to(device)
-        states = states.to(device)
-        actions = actions.to(device)
-        log_probs = log_probs.to(device)
-        returns = returns.to(device)
-        advantages = advantages.to(device)
-        advantages = advantages.detach()
-        returns = returns.detach()
+            obs,_=env.reset()
+            episode_reward = 0
+            low = torch.tensor(env.action_space.low).to(device)
+            high = torch.tensor(env.action_space.high).to(device)
+            for i in range(4096):
+                with torch.no_grad():
+                    obs=torch.from_numpy(obs).float().to(device=device)
+                    obs=normilizer.update(obs)
+                    mu,std=policy.forward(obs)
+                    dist=torch.distributions.Normal(mu,std)
+                    action=dist.sample()
+                    
+                    
+                    log_prob = dist.log_prob(action).sum(dim=-1)
+                    value=value_func.forward(obs)
+                    states.append(obs)
+                    actions.append(action.cpu().numpy())
+                    log_probs.append(log_prob.detach())
+                    values.append(value.squeeze().detach())
+                    action_env = torch.clamp(action, low, high).cpu().numpy()
+                    obs, reward, terminated, truncated, _=env.step(action_env)
+                    
+                    rewards.append(reward)
+                    episode_reward+=reward
+                    trunkateds.append(truncated)
+                    terminateds.append(terminated)
+                    if terminated or truncated:
+                        
+                        episode_count += 1
+                        
+                        obs,_=env.reset()
+                        
+                        if episode_count%20==0:
+                            print(f"Episode {episode_count} reward: {episode_reward}")
+                        episode_reward = 0
 
 
-        update_policy(states,actions,log_probs,returns,advantages)
-    
-    torch.save({
-        'model_state_dict': policy.state_dict(),
-        'obs_mean': normilizer.mean,
-        'obs_m2': normilizer.m2,
-        'obs_k': normilizer.k
-    }, "ppo_pendulum_final.pth")
+            values = torch.stack(values)
+
+            with torch.no_grad():
+                final_obs_tensor = torch.from_numpy(obs).float().to(device)
+                variance = normilizer.m2 / normilizer.k
+                std = torch.sqrt(variance + 1e-8)
+                normed_last_obs = torch.clamp((final_obs_tensor - normilizer.mean) / std, -5, 5)
+                last_value = value_func(normed_last_obs).squeeze()
+
+            advantages=compute_advanteges(values,rewards,terminateds,trunkateds,last_value)
+            returns = advantages + values
+            advantages=(advantages-advantages.mean())/(advantages.std()+1e-8)
+            states=torch.stack(states)
+            actions = torch.from_numpy(np.array(actions)).to(torch.float32).to(device)
+            log_probs=torch.stack(log_probs)
+            policy.to(device)
+            value_func.to(device)
+            states = states.to(device)
+            actions = actions.to(device)
+            log_probs = log_probs.to(device)
+            returns = returns.to(device)
+            advantages = advantages.to(device)
+            advantages = advantages.detach()
+            returns = returns.detach()
+
+
+            update_policy(states,actions,log_probs,returns,advantages)
+        
+        torch.save({
+            'model_state_dict': policy.state_dict(),
+            'obs_mean': normilizer.mean,
+            'obs_m2': normilizer.m2,
+            'obs_k': normilizer.k
+        }, "ppo_continous_final.pth")
+    except KeyboardInterrupt:
+        if input("save?(y/n)\n")=="y":
+            torch.save({
+                'model_state_dict': policy.state_dict(),
+                'obs_mean': normilizer.mean,
+                'obs_m2': normilizer.m2,
+                'obs_k': normilizer.k
+            }, "ppo_continous_final.pth")
+            print("saved")
 elif task=="test":
     env=gym.make(env_name,render_mode="human")
     checkpoint = torch.load("ppo_continous_final.pth", map_location=device)
@@ -196,17 +215,20 @@ elif task=="test":
     policy = policy.to(device)
     policy.load_state_dict(checkpoint['model_state_dict'])
     policy.eval()
-    normilizer.mean=checkpoint["obs_mean"]
-    normilizer.m2=checkpoint["obs_m2"]
-    normilizer.k=checkpoint["obs_k"]
+    mean=checkpoint["obs_mean"]
+    m2=checkpoint["obs_m2"]
+    k=checkpoint["obs_k"]
+    variance=m2/k
+    std=torch.sqrt(variance+1e-8)
     obs, _ = env.reset()
+
     
     for _ in range(1000):
         obs=torch.tensor(obs,dtype=torch.float32).to(device)
-        norm_obs = normilizer.update(obs)
+        norm_obs=(obs-mean)/std
+        norm_obs=torch.clamp(norm_obs, -5, 5)
         
         with torch.no_grad():
-            # Use mu (mean) for deterministic testing
             action_tensor,_ = policy(norm_obs)
             action_numpy = action_tensor.cpu().numpy().flatten()
         
@@ -216,3 +238,4 @@ elif task=="test":
             obs, _ = env.reset()
 
     env.close()
+    
